8c84abbd51d8a3cf75b1370639383164972c2a91
FCedi <cedricfrutiger@gmx.ch>
2025-04-15 14:52:52 +0200

    added comments

--- Test print streamlit.py
+++ Test print streamlit.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.write("It works!!")
 
 print("Hello World")
-
-print("hello again")
+#If you write "st.write("")", you will will print something in streamlit, "print("")" will retunr nothing!
+#everything else will be a background operation
