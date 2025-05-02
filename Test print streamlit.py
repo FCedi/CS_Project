ae1628424803e06552ba1dafeaee3058bf6f3b32
FCedi <cedricfrutiger@gmx.ch>
2025-05-02 13:50:57 +0200

    remove old

--- Test print streamlit.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.write("It works!!")
-
-print("Hello World")
-#If you write "st.write("")", you will will print something in streamlit, "print("")" will retunr nothing!
-#everything else will be a background operation
