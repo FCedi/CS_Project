f71537d2b266e5e8ee7970b69bfe7873aa28e080
Gabin-A <gabin.angeloz@student.unisg.ch>
2025-05-08 14:22:40 +0200

    color fix

--- Ammenities_to_app_combine.py
+++ Ammenities_to_app_combine.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.set_page_config(page_title='Local Amenities Finder', layout='centered')
-st.title('Local Amenities :[red]Finder')
+st.title('Local Amenities :red[Finder]')
