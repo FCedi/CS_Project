e0d703a5174a1982a5bbe62dac6772b77f99fcd4
Gabin-A <gabin.angeloz@student.unisg.ch>
2025-05-08 14:33:52 +0200

    Test of input cases

--- Ammenities_to_app_combine.py
+++ Ammenities_to_app_combine.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
+#set page title using https://docs.streamlit.io/ examples
 st.set_page_config(page_title='Local Amenities Finder', layout='centered')
 st.title('Local Amenities :red[Finder]')
+
+#adress input section using https://docs.streamlit.io/ examples
+st.header('Enter Your Address')
+col1, col2 = st.columns([2, 1])
+col1.text_input('Street')
+col2.text_input('House Number')
